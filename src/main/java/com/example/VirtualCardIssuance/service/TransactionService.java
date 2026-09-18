3833dadbdeca8b4e73bf1813493894994fc0feea
Abhisheka Kankumbkar <abhikankum07@gmail.com>
2026-09-18 15:39:11 +0530

    Committing VirtualCardIssuance application
    Signed-off-by: Abhisheka Kankumbkar <abhikankum07@gmail.com>

--- src/main/java/com/example/VirtualCardIssuance/service/TransactionService.java
+++ src/main/java/com/example/VirtualCardIssuance/service/TransactionService.java
@@ -1,55 +1,9 @@
 package com.example.VirtualCardIssuance.service;
 
 import com.example.VirtualCardIssuance.dto.TransactionResponse;
-import com.example.VirtualCardIssuance.entity.Transaction;
-import com.example.VirtualCardIssuance.exception.CardNotFoundException;
-import com.example.VirtualCardIssuance.repository.TransactionRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-@Service
-@Slf4j
-public class TransactionService {
-
-    private final TransactionRepository transactionRepository;
-
-
-    public TransactionService(TransactionRepository transactionRepository){
-        this.transactionRepository = transactionRepository;
-    }
-
-    public void saveTransaction(Transaction transaction){
-        transactionRepository.save(transaction);
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void saveFailedTransaction(Transaction transaction){
-        transactionRepository.save(transaction);
-    }
-
-    public List<TransactionResponse> retrieveTransactions(Long cardId){
-        log.info("Retrieve Transactions cardId{}", cardId);
-        List<Transaction> transactions =  transactionRepository.findByCardId(cardId);
-        if(transactions.isEmpty()){
-            throw new CardNotFoundException("Card not found for cardID "+cardId);
-        }
-        List<TransactionResponse> txResp = new ArrayList<>();
-        if(!transactions.isEmpty()) {
-            txResp = transactions.stream().map(t -> new TransactionResponse(t.getId(), t.getCardId(), t.getAmount(),
-                    t.getCreatedAt(), t.getType(), t.getStatus())).collect(Collectors.toList());
-        }
-        log.info("Retrieve Transactions txResp{}", txResp);
-           return txResp;
-    }
-
-    public Optional<Transaction> findByIdempotencyKey(String idempotencyKey) {
-        return transactionRepository.findByIdempotencyKey(idempotencyKey);
-    }
+public interface TransactionService {
+    public List<TransactionResponse>  retrieveTransactions(Long cardId);
 }
